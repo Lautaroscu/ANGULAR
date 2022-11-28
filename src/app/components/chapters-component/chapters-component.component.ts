32ff017435a4c6def1078ade8dfee7729eead03b
Lautaroscu <103766108+Lautaroscu@users.noreply.github.com>
2022-11-28 15:54:43 -0300

    1 file modified

--- src/app/components/chapters-component/chapters-component.component.ts
+++ src/app/components/chapters-component/chapters-component.component.ts
@@ -1,34 +1,28 @@
-import { Component, OnInit} from '@angular/core';
-import { PageEvent } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { ChaptersDataService } from '../../services/chapters-data.service';
 import { Chapters } from './interf-chapters';
 
 @Component({
   selector: 'app-chapters-component',
   templateUrl: './chapters-component.component.html',
-  styleUrls: ['./chapters-component.component.scss']
+  styleUrls: ['./chapters-component.component.scss'],
 })
 export class ChaptersComponentComponent implements OnInit {
-  chapters:Chapters[] = [] ;
-  page_size:number = 10 ;
-  page_number:number = 1;
+  chapters: Chapters[] = [];
+  page_size: number = 10;
+  page_number: number = 1;
 
-  handlePage(e:PageEvent):void{
-    this.page_size = e.pageSize ;
-    this.page_number = e.pageIndex+1 ;
+  handlePage(e: PageEvent): void {
+    this.page_size = e.pageSize;
+    this.page_number = e.pageIndex + 1;
   }
-  
 
-  constructor(private DataService:ChaptersDataService) {
-    
-   }
+  constructor(private DataService: ChaptersDataService) {}
 
   ngOnInit(): void {
-      this.DataService.getAll()
-      .subscribe(chapters => this.chapters = chapters) ;
-     
+    this.DataService.getAll().subscribe(
+      (chapters) => (this.chapters = chapters)
+    );
   }
-
- 
-
 }
